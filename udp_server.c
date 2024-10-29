7769f171eee9ce0a8401de204d8c75408cc710e0
KarpoKoodaa <karpokoodaa@gmail.com>
2024-10-29 17:41:25 +0200

    Added functionality for multiple connections

--- udp_server.c
+++ udp_server.c
@@ -40,24 +40,46 @@ int main() {
     }
     freeaddrinfo(bind_address);
 
-    struct sockaddr_storage client_address;
-    socklen_t client_len = sizeof(client_address);
-    char read[1024];
-    int bytes_received = recvfrom(socket_listen, read, 1024, 0, (struct sockaddr*) &client_address, &client_len);
+    fd_set master;
+    FD_ZERO(&master);
+    FD_SET(socket_listen, &master);
+    SOCKET max_socket = socket_listen;
 
-    printf("Received (%d bytes): %.*s\n", bytes_received, bytes_received, read);
+    printf("Waiting for connections....\n");
 
-    printf("Remote address is: ");
-    char address_buffer[100];
-    char service_buffer[100];
-    getnameinfo(((struct sockaddr*)&client_address),
-            client_len,
-            address_buffer, sizeof(address_buffer),
-            service_buffer, sizeof(service_buffer),
-            NI_NUMERICHOST | NI_NUMERICSERV);
-    printf("%s %s\n", address_buffer, service_buffer);
+    while (1) {
+        fd_set reads;
+        reads = master;
+        if(select(max_socket +1, &reads, 0, 0, 0) < 0) {
+            fprintf(stderr, "select() failed. (%d)\n", GETSOCKETERRNO());
+            return 1;
+        }
 
-    CLOSESOCKET(socket_listen);
+        if (FD_ISSET(socket_listen, &reads)) {
+            struct sockaddr_storage client_address;
+            socklen_t client_len = sizeof(client_address);
+
+            char read[1024];
+            int bytes_received = recvfrom(socket_listen, read, 1024, 0, (struct sockaddr *)&client_address, &client_len);
+            if (bytes_received < 1) {
+                fprintf(stderr, "connection closed. (%d)\n", GETSOCKETERRNO());
+                return 1;
+            }
+
+            printf("Received (%d bytes): %.*s\n", bytes_received, bytes_received, read);
+            
+            printf("Remote address is: ");
+            char address_buffer[100];
+            char service_buffer[100];
+            getnameinfo(((struct sockaddr*)&client_address),
+                    client_len,
+                    address_buffer, sizeof(address_buffer),
+                    service_buffer, sizeof(service_buffer),
+                    NI_NUMERICHOST | NI_NUMERICSERV);
+            printf("%s %s\n", address_buffer, service_buffer);
+        }
+    }
+   
 
     printf("Finished.\n");
 
